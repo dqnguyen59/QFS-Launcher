c43584a3e6b801ac3662cf2f898f4b3a5dd39de7
dqnguyen59 <d.q.nguyen@smartblackbox.nl>
2023-01-03 19:01:01 +0100

    Signed-off-by: dqnguyen59 <d.q.nguyen@smartblackbox.nl>

--- src/main/java/org/smartblackbox/launcher/QFS.java
+++ src/main/java/org/smartblackbox/launcher/QFS.java
@@ -1,8 +1,12 @@
 package org.smartblackbox.launcher;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class QFS {
 
@@ -25,17 +29,11 @@ public class QFS {
 		return getOsName().startsWith("Linux");
 	}
 	
-	public static void main(String[] args) {
-		System.out.println("Quantum Field Simulator Launcher");
-
-		try {
-			runCommand("java -jar -XstartOnFirstThread QuantumFieldSimulator.jar");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
+	public static boolean fileExists(String fileName) {
+		File file = new File(fileName);
+		return file.exists();
 	}
-
+	
 	public static String runCommand(String command, String suffix) throws Exception {
 		try {
 			Process p = Runtime.getRuntime().exec(command);
@@ -70,6 +68,42 @@ public class QFS {
 
 	public static String runCommand(String command) throws Exception {
 		return runCommand(command, "\n");
+	}
+
+	private static String getPlatform() {
+		if (isWindows())
+			return "Windows";
+		else if (isMac())
+			return "MacOS";
+		else if (isLinux())
+			return "Linux";
+		else 
+			return "Unknown";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Quantum Field Simulator Launcher");
+
+		try {
+			String platform = getPlatform();
+			System.out.println("OS detected: " + platform);
+			String appFile = "QuantumFieldSimulator-" + platform + ".jar";
+			String cmd = "java -jar " + (isMac()? "-XstartOnFirstThread " : "") + appFile;
+			
+			if (fileExists(appFile)) {
+				System.out.println("Run command: " + cmd);
+				runCommand(cmd);
+			}
+			else {
+				JFrame frame = new JFrame();
+				JOptionPane.showMessageDialog(frame, "File '" + appFile + "' not exists!",
+						"Dialog", JOptionPane.ERROR_MESSAGE);
+				frame.dispose();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
